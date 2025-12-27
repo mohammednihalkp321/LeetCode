223076467f963ab2ace056407304914f1f197cc7
mohammednihalkp321 <nihalkp897@gmail.com>
2025-12-27 12:18:48 +0530

    Time: 921 ms (44.86%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,13 @@
+select
+    s.product_id,
+    s.year as first_year ,
+    s.quantity ,
+    s.price
+from sales s
+join (
+    select product_id,min(year) as first_year
+    from sales
+    group by product_id
+) fy
+on s.product_id = fy.product_id
+and s.year = fy.first_year;
