73cfffbbc74d28917d81c0e717a112c980e5be76
mohammednihalkp321 <nihalkp897@gmail.com>
2026-01-20 10:37:01 +0530

    Time: 379 ms (5.12%), Space: 0B (100%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,11 @@
+select 
+    p.product_name,
+    sum(o.unit) as unit
+from products p
+left join orders o
+   on p.product_id = o.product_id
+where o.order_date >= '2020-02-01'
+    and o.order_date < '2020-03-01'
+group by p.product_name
+having sum(o.unit) >= 100 ;    
+
