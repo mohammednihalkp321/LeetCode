49b932f3927cc95e5fd9bfa31dad71e39243f879
MOHAMMED NIHAL KP <nihalkp897@gmail.com>
2026-09-03 11:30:27 +0530

    Time: 253 ms (78.34%), Space: 67.7 MB (14.52%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    salary = employee["salary"].drop_duplicates().nlargest(2)
+
+    return pd.DataFrame({
+        "SecondHighestSalary": [salary.iloc[-1] if len(salary) == 2 else None]
+    })
+    
