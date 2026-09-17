14dee28ef4e28440e035630267bebc86062d1d71
MOHAMMED NIHAL KP <nihalkp897@gmail.com>
2026-09-17 15:33:59 +0530

    Time: 415 ms (6.65%), Space: 68.7 MB (17.66%) - LeetHub

---
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    
+    result = activities.groupby('sell_date').agg(
+        num_sold=('product', 'nunique'),
+        products=('product', lambda x: ','.join(sorted(x.unique())))
+    ).reset_index()
+    
+    return result.sort_values('sell_date')
